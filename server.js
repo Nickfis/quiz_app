67db9e94c92a329e1c0c1296744abe682ddd641b
Niklas Fischer <niklas.fischer@berlin.de>
2020-12-18 15:06:24 +0100

    make it ready for deployment

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const http = require("http");
+const app = express();
+const server = require("http").Server(app);
+
+app.use(express.static("/client/build"));
 const socketIo = require("socket.io");
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
-
-const app = express();
-app.use(index);
-
-const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
