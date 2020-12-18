f03494c7fb5baf2f2ca5c7398f55fc781a5ee977
Niklas Fischer <niklas.fischer@berlin.de>
2020-12-18 10:00:14 +0100

    creating backend for socket.io

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require("express");
-// const connectDB = require("./config/db");
-// const config = require("config");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
 const app = express();
-// connect db
-// connectDB();
+app.use(index);
 
-app.use(express.json({extended: false}));
+const server = http.createServer(app);
 
-app.get("/", (req, res) => res.send("API running"));
-const PORT = process.env.PORT || 5000;
+const io = socketIo(server);
 
-app.listen(PORT, console.log(`server started on port ${PORT}`));
+let interval;
+
+io.on("connection", socket => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
